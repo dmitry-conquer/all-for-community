0c6027cd793ed00ee1e4f2c211c14a3be5db235b
Dmitry Conquer <dmitry.conquer@gmail.com>
2025-02-27 16:23:07 +0200

    27/02/2025 16:22 | Header + base hero

--- src/scripts/components/Header.ts
+++ src/scripts/components/Header.ts
@@ -4,6 +4,7 @@ export class Header {
     overlay: "[data-js-header-overlay]",
     buttonOpen: "[data-js-header-button-open]",
     buttonClose: "[data-js-header-button-close]",
+    menuItem: ".menu-item-has-children > a",
   };
 
   private readonly stateClasses: Record<string, string> = {
@@ -19,12 +20,14 @@ export class Header {
   private overlayElement: HTMLElement | null;
   private buttonOpenElement: HTMLElement | null;
   private buttonCloseElement: HTMLElement | null;
+  private menuItems: Element[];
 
   constructor() {
     this.rootElement = document.querySelector(this.selectors.root);
     this.overlayElement = this.rootElement?.querySelector(this.selectors.overlay) || null;
     this.buttonOpenElement = this.rootElement?.querySelector(this.selectors.buttonOpen) || null;
     this.buttonCloseElement = this.rootElement?.querySelector(this.selectors.buttonClose) || null;
+    this.menuItems = [...(this.rootElement?.querySelectorAll(this.selectors.menuItem) || [])];
     this.bindEvents();
   }
 
@@ -38,20 +41,43 @@ export class Header {
     this.buttonCloseElement?.setAttribute(this.attributes.ariaExpanded, state.toString());
   }
 
+  isNotOverlayRelated(event: MouseEvent) {
+    return (
+      !this.overlayElement?.contains(event.target as Node) && !this.buttonOpenElement?.contains(event.target as Node)
+    );
+  }
+
   onClickOutside = (event: MouseEvent): void => {
-    if (
-      !this.overlayElement?.contains(event.target as Node) &&
-      !this.buttonOpenElement?.contains(event.target as Node) &&
-      !this.buttonOpenElement?.contains(event.target as Node)
-    ) {
+    if (this.isNotOverlayRelated(event)) {
       this.toggleMenu(false);
     }
   };
+
+  onMenuItemClick(currentIndex: number) {
+    this.menuItems.forEach((menuItem, index) => {
+      const subMenu = menuItem.nextElementSibling as HTMLElement;
+      const isActive = menuItem.classList.contains(this.stateClasses.isActive);
+
+      if (currentIndex === index) {
+        menuItem.classList.toggle(this.stateClasses.isActive);
+        subMenu.style.maxHeight = isActive ? "" : `${subMenu.scrollHeight}px`;
+      } else {
+        menuItem.classList.remove(this.stateClasses.isActive);
+        subMenu.style.maxHeight = "";
+      }
+    });
+  }
 
   bindEvents(): void {
     this.buttonOpenElement?.addEventListener("click", () => this.toggleMenu(true));
     this.buttonCloseElement?.addEventListener("click", () => this.toggleMenu(false));
     document.documentElement.addEventListener("click", this.onClickOutside);
+    this.menuItems.forEach((item, index) => {
+      item.addEventListener("click", e => {
+        e.preventDefault();
+        this.onMenuItemClick(index);
+      });
+    });
   }
 }
 
--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,8 +1,10 @@
 import "../styles/main.scss";
 import Header from "./components/Header";
 import Sliders from "./components/Sliders";
+import TabsCollection from "./components/Tabs";
 
 document.addEventListener("DOMContentLoaded", () => {
   new Sliders();
   new Header();
+  new TabsCollection();
 });
