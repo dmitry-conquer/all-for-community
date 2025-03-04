30bb17dcf8e09d44e7d2d06b29899e495dcf8249
Dmitry Conquer <dmitry.conquer@gmail.com>
2025-03-04 12:51:37 +0200

    Add new styles and components for grid and not found sections; enhance button styles and integrate smooth scrolling

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,10 +1,26 @@
+declare const acfData: {
+  smoothScroll?: boolean;
+};
+
 import "../styles/main.scss";
+import "aos/dist/aos.css";
+
 import Header from "./components/Header";
 import Sliders from "./components/Sliders";
-import TabsCollection from "./components/Tabs";
+import Lenis from "lenis";
+import AOS from "aos";
 
 document.addEventListener("DOMContentLoaded", () => {
   new Sliders();
   new Header();
-  new TabsCollection();
+  AOS.init({
+    once: true,
+    duration: 1000,
+  });
+
+  if (acfData.smoothScroll) {
+    new Lenis({
+      autoRaf: true,
+    });
+  }
 });
